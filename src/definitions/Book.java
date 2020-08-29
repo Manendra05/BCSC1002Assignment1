f336c85a81259b382cfa729a9155e221e9059283
manendra <manendra.singh_cs19@gla.ac.in>
2020-08-29 21:22:02 +0530

    add getter method

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -17,7 +17,28 @@ public class Book {
         this.bookISBNNumber = "978-8176566216";
     }
 
-}
+    public Book(String bookName, String authorName, String bookISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.bookISBNNumber = bookISBNNumber;
+
+
+    }
+
+    /* Now creat getter method for the fields
+
+     */
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getBookISBNNumber() {
+        return bookISBNNumber;
+    }
 
 
 }
