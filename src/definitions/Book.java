981f13bed2420eb5ab5927dd11b3effb269fd37b
manendra <manendra.singh_cs19@gla.ac.in>
2020-08-29 19:29:58 +0530

    Add book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,14 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Manendra Singh
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
- * */
+ **/
 package definitions;
 
+import java.util.objects;
+
 public class Book {
+
+}
 }
