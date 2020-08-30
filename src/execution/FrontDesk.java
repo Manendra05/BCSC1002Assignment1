3195951315db90e9720d5d3b26369e831d5931c3
manendra <manendra.singh_cs19@gla.ac.in>
2020-08-30 21:18:22 +0530

    creat fields in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,6 +7,10 @@
 package execution;
 
 public class FrontDesk {
+    public static final int EXIT = 4;
+    public static final int ISSUE_A_BOOK = 1;
+    public static final int RETURN_PREVIOUSLY_ISSUED_BOOKS = 2;
+    public static final int SHOW_ALL_BOOKS_ISSUED = 3;
 
-    }
 
+}
