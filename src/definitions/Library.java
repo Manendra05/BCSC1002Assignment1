227d4c59ac8224881e9770c68f6c20e41b1de5e1
manendra <manendra.singh_cs19@gla.ac.in>
2020-08-30 09:40:10 +0530

    add constrictor

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Manendra Singh(dbc2201)
+ *  User: Manendra Singh
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
@@ -7,4 +7,14 @@
 package definitions;
 
 public class Library {
+    private Book[] availableBooks;
+
+    public Library() {
+        this.availableBooks = new Book[500];
+        for (int i = 0; i < availableBooks.length; i++) {
+            this.availableBooks[i] = new Book("Maths", "RD Sharma", "346124643256846");
+            this.availableBooks[i] = new Book("physics", "HC Verma", "525862576451");
+        }
+
+    }
 }
