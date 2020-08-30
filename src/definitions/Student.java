2ec0c3a156cdc7a629266e4287350309fc24db3a
manendra <manendra.singh_cs19@gla.ac.in>
2020-08-30 12:52:52 +0530

    created getter method

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -9,8 +9,38 @@ package definitions;
 public class Student {
     private String firstName;
     private String middleName;
-    private String lastNameOfStudent;
-    private long UniversityRollNo;
+    private String lastName;
+    private int universityRollNo;
     private int numberOfBooksIssued;
     private Book[] BooksIssued;
 }
+
+    public Student(String firstName, String middleName, String lastName,
+                   int universityRollNo, int numberOfBooksIssued) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.universityRollNo = universityRollNo;
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    public Student() {
+        this.BooksIssued = new Book[500];
+    }
+    // creat getter method
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+}
+
+
